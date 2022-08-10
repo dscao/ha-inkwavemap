c9013b0db71bd8252e23329340532e34a37dd30b
dscao <dscao.com@gmail.com>
2022-08-10 16:13:54 +0800

    高德安全密钥
    增加高德API key的安全密钥选项

--- www/custom_ui/inkwavemap/config.js
+++ www/custom_ui/inkwavemap/config.js
@@ -9,6 +9,14 @@ HomeAssistantWebAPIUrl="./../../.."
 // (必填)
 GaodeMapKey="cc9dxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
+// 高德API key的安全密钥
+// 可获取到key值和安全密钥jscode（自2021年12月02日升级，升级之后所申请的 key 必须配备安全密钥 jscode 一起使用)
+// 注意：此次升级不会影响之前已获得 key 的使用；升级之后的新增的key必须要配备安全密钥一起使用（不需要则留空）
+GaodeMapjscode=""
+// 如果使用高德强烈推荐的方式一：JSAPI key搭配代理服务器并携带安全密钥转发（安全）（不使用则留空）
+GaodeMapserviceHost:""
+//以上两者都不为空时，以GaodeMapjscode优先。
+
 
 // 永久Token(HomeAssistant 0.77.2版本以上时可用)
 // HomeAssistant 0.77.2版本以后，HomeAssistant登录后如果未启用记住密码，墨澜地图将无法自行获取Token（配置了正确的LongTimeToken后可以正常使用）
